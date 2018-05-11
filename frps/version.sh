dc947d537462fe154f10e1acb0cded612256f62c
clangcn <clangcn@gmail.com>
2018-05-11 14:49:24 +0800

    new

---
+++ frps/version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export FRPS_VER=0.16.1
+export FRPS_INIT="https://raw.githubusercontent.com/clangcn/onekey-install-shell/master/frps/frps.init"
+export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
+export github_download_url="https://github.com/fatedier/frp/releases/download"
