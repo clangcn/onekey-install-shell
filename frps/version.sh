e201b2e9440920323f511a65d574cd22b17fb235
woytu <admin@woytu.com>
2018-10-16 17:44:14 +0800

    Update version.sh

--- frps/version.sh
+++ frps/version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export FRPS_VER=0.21.0
 export FRPS_INIT="https://raw.githubusercontent.com/woytu/onekey-install-shell/master/frps/frps.init"
-export aliyun_download_url="https://code.aliyun.com/woytu/frp/raw/master"
+export aliyun_download_url="https://code.aliyun.com/woytu/clangcn/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
